0dc2eb7bea7605899c1990ab4d8e650cd2f3e782
tonasun <tona.sun@meetsocial.cn>
2020-01-07 19:15:34 +0800

    @Import(MyMapperScannerRegistrar.class)手写了一个mapperscan用于refresh代码阅读

--- spring-yang/src/main/java/com/yang/conf/MyMapperScannerRegistrar.java
+++ spring-yang/src/main/java/com/yang/conf/MyMapperScannerRegistrar.java
@@ -3,7 +3,9 @@ package com.yang.conf;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.type.AnnotationMetadata;
 
 /**
@@ -12,15 +14,22 @@ import org.springframework.core.type.AnnotationMetadata;
  * @Author: tona.sun
  * @Date: 2020/01/07 11:04
  */
-public class MyMapperScannerRegistrar implements ImportBeanDefinitionRegistrar {
+public class MyMapperScannerRegistrar implements ImportBeanDefinitionRegistrar, ResourceLoaderAware {
+	private ResourceLoader resourceLoader;
 
 	@Override
 	public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+		System.out.println(resourceLoader);
 		BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition();
 		AbstractBeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
 		//TODO 这里beanDefinition的BeanClass类型是MyBeanFactory，但是会放进去getObject
 		beanDefinition.setBeanClass(MyBeanFactory.class);
 		beanDefinition.getConstructorArgumentValues().addGenericArgumentValue("com.yang.dao.mapper.UserMapper");
 		registry.registerBeanDefinition("userMapper", beanDefinition);
+	}
+
+	@Override
+	public void setResourceLoader(ResourceLoader resourceLoader) {
+		this.resourceLoader = resourceLoader;
 	}
 }
